2313b199c43f2d39e92b9d284b8f52dc4fd24d45
unknown <udarasandaruwan96@gmail.com>
2022-02-11 20:44:47 +0530

    103complete

--- frontend/src/components/imports/BaseLogin.jsx
+++ frontend/src/components/imports/BaseLogin.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-//import {useLocation,useHistory,Link} from 'react-router-dom';
+import {useLocation,useHistory,Link} from 'react-router-dom';
 
 
 
 export default function BaseLogin() {
 
-    // const location = useLocation();
-    // const history = useHistory();
+    const location = useLocation();
+    const history = useHistory();
+      
+    let defaultClass = "nav-link link-btn btn-primary default-bg";
+    let active = " active"
 
-    // const register =() =>history.push("/register")
-    // const login =() =>history.push("/login")
+    const register =() =>history.push("/register")
+    const login =() =>history.push("/login")
 
     return(
       <div className='col-sm-5 bg-img align-self-center'> 
          <div className='info'>
              <div className='logo clearfix'>
-                <a href="#" className='nav-brand'>Logo</a>
+                 <Link className='nav-brand' to = "/">Logo</Link>
+                
              </div>
              <div className="btn-section">
-                 <button className='nav-link link-btn btn-primary default-bg'> Login</button>
-                 <button  className='nav-link link-btn btn-primary default-bg'> Register</button>
+                 <button onClick={login} className= {location.pathname === "/login"? defaultClass + active : defaultClass}> Login</button>
+                 <button onClick={register} className= {location.pathname === "/register"? defaultClass + active : defaultClass}> Register</button>
              </div>
          </div>
       </div>
